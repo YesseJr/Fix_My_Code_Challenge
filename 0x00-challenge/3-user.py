e3719e19a536f8fc3031e391a176d18540553be7
YesseJr <thonnieyesse@hotmail.com>
2022-02-12 17:57:37 +0300

    upper() method was the disaster there...

--- 0x00-challenge/3-user.py
+++ 0x00-challenge/3-user.py
@@ -41,6 +41,7 @@ class User():
             self.__password = None
         else:
             self.__password = hashlib.md5(pwd.encode()).hexdigest().lower()
+            # Did you see the upper() method in the above line???
 
     def is_valid_password(self, pwd):
         """
